0beb35bf499ba6f27e7f369321fa44b55696819f
arief setia <arief.setia18@yahoo.co.id>
2017-04-11 01:31:24 +0900

    Add files via upload

---
+++ latarray4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+string nama[3];
+
+int main (){
+	 for (int i=0; i< 3; i++)
+		 cin >> nama[i];
+	     
+		 cout << nama[0] << " _ " << nama[1] << endl;
+		 cout << nama[0] << " _ " << nama[2] << endl;
+		 return 0;
+		 
+}
